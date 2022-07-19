6dc460922e4c24050c4e904be260840d541c687a
mihir <mewadamihir@gmail.com>
2022-07-20 00:04:24 +0530

    small fix typo error

--- react-table-crud-example-master/src/index.js
+++ react-table-crud-example-master/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,4 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-reportWebVitals();
+
